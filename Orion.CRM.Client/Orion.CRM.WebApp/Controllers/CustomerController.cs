6818644e3120f05893dfa459f7403c07bada1a8b
gaochong <neptunecoon@gmail.com>
2019-01-28 12:15:08 +0800

    patch 2.0例行提交

---
+++ Orion.CRM.Client/Orion.CRM.WebApp/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Orion.CRM.WebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult My()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
